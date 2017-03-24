c79af8d70c80780a63ff5fb7c5c189d17e743594
Ayan Acharyya <acharyyaayan@gmail.com>
2017-03-24 16:17:24 +1100

    Cleaned, user input enabled (see example_using_ayans_EW_fitter.txt)

--- dftolatex.py
+++ dftolatex.py
@@ -5,22 +5,35 @@ import pandas as pd
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 50)
 pd.set_option('display.width', 1000)
-'''
-input_path = '/Users/acharyya/Work/astro/mageproject/AYAN_Code/' #'/Users/acharyya/Dropbox/MagE_atlas/Contrib/EWs/' #where is the dataframe resides
-infile = 'fitted_line_list.txt'
-output_path = '/Users/acharyya/Documents/writings/papers/abundance_pap/'
-outfile = 'stacked_lineEW'
-galaxy = 'magestackbyneb'
-'''
-input_path = '/Users/acharyya/Documents/writings/papers/abundance_pap/' #'/Users/acharyya/Dropbox/MagE_atlas/Contrib/EWs/' #where is the dataframe resides
-infile = 'lineflux_restUV.txt'
-output_path = '/Users/acharyya/Documents/writings/papers/abundance_pap/'
-outfile = 'lineflux_restUV_detected'
-galaxy = 'rcs0327-E'
-
-line_table = pd.read_table(input_path+infile, delim_whitespace=True, comment="#") #input dataframe file
-line_table = line_table[line_table['label'].eq(galaxy)]
-thresh = 3
+import argparse as ap
+import os
+HOME = os.getenv('HOME')+'/'
+#-----------------------------------------------
+parser = ap.ArgumentParser(description="Mage spectra fitting tool")
+parser.add_argument("--infile")
+parser.add_argument("--outfile")
+parser.add_argument("--shortlabel")
+parser.add_argument("--thresh")
+args, leftovers = parser.parse_known_args()
+if args.infile is not None:
+    infile = args.infile
+else:
+    infile = HOME+'Documents/writings/papers/abundance_pap/lineflux_restUV.txt' #ffull-path-name of input file
+if args.outfile is not None:
+    outfile = args.outfile
+else:
+    outfile = infile[:-4]+'_detected'
+if args.shortlabel is not None:
+    shortlabel = args.shortlabel
+else:
+    shortlabel = 'rcs0327-E'
+if args.thresh is not None:
+    thresh = float(args.thresh)
+else:
+    thresh = 3.
+#-----------------------------------------------
+line_table = pd.read_table(infile, delim_whitespace=True, comment="#") #input dataframe file
+line_table = line_table[line_table['label'].eq(shortlabel)]
 
 tab=line_table[['line_lab','rest_wave','EWr_fit','EWr_fit_u','EW_signi','EWr_Suplim','f_redcor','f_line_u','f_Suplim_redcor']]  
 tab.EW_signi=tab.EW_signi.astype(np.float64) 
@@ -28,8 +41,8 @@ tab.EWr_Suplim=tab.EWr_Suplim.astype(np.str)
 tab.f_Suplim_redcor=tab.f_Suplim_redcor.map('{:,.3e}'.format).astype(np.str) 
 tab.EWr_fit=tab.EWr_fit.astype(np.str) 
 tab.f_redcor=tab.f_redcor.map('{:,.3e}'.format).astype(np.str)
-tab.EWr_fit = np.where(tab.EW_signi<thresh,'< '+tab.EWr_Suplim,tab.EWr_fit) 
-tab.f_redcor = np.where(tab.EW_signi<thresh,'< '+tab.f_Suplim_redcor,tab.f_redcor) 
+tab.EWr_fit = np.where(tab.EW_signi<thresh,'<'+tab.EWr_Suplim,tab.EWr_fit) 
+tab.f_redcor = np.where(tab.EW_signi<thresh,'<'+tab.f_Suplim_redcor,tab.f_redcor) 
 tab.EWr_fit_u=tab.EWr_fit_u.astype(np.str) 
 tab.f_line_u=tab.f_line_u.astype(np.str) 
 tab.EWr_fit_u[tab.EW_signi<thresh] = None 
@@ -43,5 +56,8 @@ tab=tab.replace('nan','-')
 tab['EWr_fit_u'].fillna(value='-', inplace = True)
 tab['f_line_u'].fillna(value='-', inplace = True)
 print tab #
-tab.to_latex(output_path+outfile+'.tex', index=None)
-tab.to_csv(output_path+outfile+'.txt', sep='\t',mode ='w', index=None)
+tab.to_latex(outfile+'.tex', index=None)
+tab.to_csv(outfile+'.txt', sep='\t',mode ='w', index=None)
+print 'Written files '+ outfile+'.tex'
+print 'and '+ outfile+'.txt'
+print 'Finished!'
